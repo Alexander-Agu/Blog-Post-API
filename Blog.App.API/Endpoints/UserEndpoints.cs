dc8b3c15a567e6cba9ad6f449db8ce4416ebaada
Alexander-Agu <theonlyagu@gmail.com>
2025-05-15 10:12:10 +0200

    Created password and email validation checks

--- Blog.App.API/Endpoints/UserEndpoints.cs
+++ Blog.App.API/Endpoints/UserEndpoints.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using Blog.App.API.Data;
 using Blog.App.API.Dtos.UserDtao;
 using Blog.App.API.Entities;
@@ -38,6 +39,9 @@ public static class UserEndpoints
             bool emailExist = await dbContext.Users.AnyAsync(x => x.Email == createUser.Email);
             if (emailExist) return Results.Conflict(new { message = "Email already exists!" });
 
+            // Validates password
+            if (!ValidatePassword(createUser.Password)) return Results.Conflict(new { message = "Create a better password!" });
+
             await dbContext.Users.AddAsync(user);
             await dbContext.SaveChangesAsync();
 
@@ -46,5 +50,32 @@ public static class UserEndpoints
         .WithParameterValidation();
 
         return group;
+    }
+
+    private static bool ValidatePassword(string password)
+    {
+        if (password.Length < 8) return false; // Checks if password is over 8 characters
+
+
+        bool number = false, special = false, lower = false, upper = false;
+
+        for (int x = 0; x <= password.Length - 1; x++)
+        {
+            if (char.IsNumber(password, x)) number = true; // Checks if password has a number
+
+            if (char.IsUpper(password, x)) upper = true; // Checks if password has an uppercase letter
+
+            if (char.IsLower(password, x)) lower = true; // Checks if password has a lowercase letter
+
+            if ("!@#$%^&*()_".Contains(password[x])) special = true; // Checks if password has special characters
+
+            if (number && special && lower && upper) break; // If all these conditions have been met stop checking
+        }
+
+        if (number && special && lower && upper)
+        {
+            return true;
+        }
+        return false;
     }
 }
